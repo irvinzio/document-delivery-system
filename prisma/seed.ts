d53249fb1fc037e0590ed1f0af7020d9e17062b1
iramirez <irvinzio.ram@gmail.com>
2025-08-12 09:58:23 -0600

    seed error fix

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,5 +1,5 @@
-import { PrismaClient } from '@prisma/client';
-import bcrypt from 'bcrypt';
+const { PrismaClient } = require('@prisma/client');
+const bcrypt = require('bcrypt');
 
 const prisma = new PrismaClient();
 
@@ -11,7 +11,7 @@ async function main() {
       { email: 'bob@example.com', password, name: 'Bob' },
       { email: 'carol@example.com', password, name: 'Carol' },
     ],
-    skipDuplicates: true,
+  skipDuplicates: true,
   });
 }
 

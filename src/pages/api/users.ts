4ec173524887a813cbf13d908294c2ec36465c1a
iramirez <irvinzio.ram@gmail.com>
2025-08-12 09:55:35 -0600

    handler to get user  to use on upload recipients

---
+++ src/pages/api/users.ts
@@ -0,0 +1,15 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  try {
+    const users = await prisma.user.findMany({
+      select: { id: true, name: true, email: true }
+    });
+    res.status(200).json({ users });
+  } catch (error) {
+    res.status(500).json({ error: "Failed to fetch users" });
+  }
+}
